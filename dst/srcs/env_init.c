ee196f05d4bd8635aabedcd5359b0bc785b9ee82
tatsuoo000 <rush.life29@gmail.com>
2021-06-23 22:25:18 +0900

    env_init修正

--- dst/srcs/env_init.c
+++ dst/srcs/env_init.c
@@ -6,17 +6,37 @@
 /*   By: tkano <tkano@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 18:44:13 by tkano             #+#    #+#             */
-/*   Updated: 2021/06/15 16:08:46 by tkano            ###   ########.fr       */
+/*   Updated: 2021/06/20 13:12:18 by tkano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		env_init(t_status *status, char **arg_env)
+static int	set_env(t_env *env, char **arg_env)
 {
-	t_env	*env;
 	t_env	*new;
 	int		i;
+
+	i = 1;
+	while (arg_env && arg_env[0] && arg_env[i])
+	{
+		new = malloc(sizeof(t_env));
+		if (!new)
+			return (ERROR);
+		new->value = ft_strdup(arg_env[i]);
+		if (!new->value)
+			return (ERROR);
+		new->next = NULL;
+		env->next = new;
+		env = new;
+		i++;
+	}
+	return (SUCCESS);
+}
+
+int	env_init(t_status *status, char **arg_env)
+{
+	t_env	*env;
 
 	env = malloc(sizeof(t_env));
 	if (!env)
@@ -26,17 +46,5 @@ int		env_init(t_status *status, char **arg_env)
 		return (ERROR);
 	env->next = NULL;
 	status->env_tab = env;
-	i = 1;
-	while (arg_env && arg_env[0] && arg_env[i])
-	{
-		new = malloc(sizeof(t_env));
-		if (!new)
-			return (ERROR);
-		new->value = ft_strdup(arg_env[i]);
-		new->next = NULL;
-		env->next = new;
-		env = new;
-		i++;
-	}
-	return (SUCCESS);
+	return (set_env(env, arg_env));
 }
